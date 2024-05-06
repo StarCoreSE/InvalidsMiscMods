c7cac728daa61ed356e474c019f5a506201de10b
Aristeas <94058548+Jnick-24@users.noreply.github.com>
2024-05-05 23:16:22 -0500

    BlockCulling now disables when inside a grid's WorldAABB

--- BlockCulling/Data/Scripts/BlockCulling/BlockCulling_Session.cs
+++ BlockCulling/Data/Scripts/BlockCulling/BlockCulling_Session.cs
@@ -10,11 +10,20 @@ using VRage.Game.ModAPI;
 using VRage.ModAPI;
 using VRageMath;
 
-namespace Block_Culling_Test.Data.Scripts.BlockCulling
+namespace Scripts.BlockCulling
 {
     [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
-    public class BlockCullingSession : MySessionComponentBase
+    public partial class BlockCulling : MySessionComponentBase
     {
+        /// <summary>
+        /// Map of all culled blocks, used for temporary distance unculling.
+        /// </summary>
+        private readonly Dictionary<IMyCubeGrid, HashSet<IMyCubeBlock>> _culledBlocks = new Dictionary<IMyCubeGrid, HashSet<IMyCubeBlock>>();
+        /// <summary>
+        /// Map of all temporarily unculled grids, set by distance.
+        /// </summary>
+        private readonly HashSet<IMyCubeGrid> _unCulledGrids = new HashSet<IMyCubeGrid>();
+
         #region Base Methods
 
         public override void LoadData()
@@ -23,7 +32,6 @@ namespace Block_Culling_Test.Data.Scripts.BlockCulling
                 return;
 
             MyAPIGateway.Entities.OnEntityAdd += OnEntityAdd;
-            MyAPIGateway.Utilities.ShowMessage("Block Culler", "Loaded.");
         }
 
         protected override void UnloadData()
@@ -32,6 +40,33 @@ namespace Block_Culling_Test.Data.Scripts.BlockCulling
                 return;
 
             MyAPIGateway.Entities.OnEntityAdd -= OnEntityAdd;
+        }
+
+        private int _drawTicks;
+        public override void Draw()
+        {
+            _drawTicks++;
+            if (MyAPIGateway.Utilities.IsDedicated || _drawTicks % 15 != 0) // Only check every 1/4 second
+                return;
+
+            Vector3D cameraPosition = MyAPIGateway.Session?.Camera?.Position ?? Vector3D.MaxValue;
+            foreach (var grid in _culledBlocks.Keys)
+            {
+                // Recull blocks if within grid's WorldAABB
+                if (grid.WorldAABB.Contains(cameraPosition) != ContainmentType.Contains)
+                {
+                    if (_unCulledGrids.Remove(grid))
+                        foreach (var block in _culledBlocks[grid])
+                            block.Visible = false;
+                    continue;
+                }
+
+                // Set blocks to visible if not already done
+                if (!_unCulledGrids.Add(grid))
+                    continue;
+                foreach (var block in _culledBlocks[grid])
+                    block.Visible = true;
+            }
         }
 
         #endregion
@@ -46,6 +81,13 @@ namespace Block_Culling_Test.Data.Scripts.BlockCulling
 
             grid.OnBlockAdded += OnBlockPlace;
             grid.OnBlockRemoved += OnBlockRemove;
+            grid.OnClose += gridEntity =>
+            {
+                _culledBlocks.Remove(grid);
+                _unCulledGrids.Remove(grid);
+            };
+
+            _culledBlocks.Add(grid, new HashSet<IMyCubeBlock>());
 
             foreach (var block in grid.GetFatBlocks<IMyCubeBlock>())
                 SetTransparency(block.SlimBlock);
@@ -62,73 +104,11 @@ namespace Block_Culling_Test.Data.Scripts.BlockCulling
             {
                 IMySlimBlock slimNeighbor = slimBlock.CubeGrid.GetCubeBlock(blockPos);
                 if (slimNeighbor?.FatBlock != null)
-                    slimNeighbor.FatBlock.Visible = true;
-            }
-        }
-
-        #endregion
-
-        #region Private Methods
-
-        private void SetTransparency(IMySlimBlock slimBlock, bool recursive = true)
-        {
-            IMyCubeBlock block = slimBlock.FatBlock;
-            if (!recursive && block == null)
-                return; // No logic would run so just return early
-
-            var blockSlimNeighbors = new List<IMySlimBlock>();
-
-            foreach (var blockPos in GetSurfacePositions(slimBlock))
-            {
-                IMySlimBlock slimNeighbor = slimBlock.CubeGrid.GetCubeBlock(blockPos);
-                if (slimNeighbor == null)
-                    continue;
-                if (slimNeighbor.FatBlock == null || !(slimNeighbor.FatBlock is IMyLightingBlock)) // Limit to slimblocks and fatblocks with physics
-                    blockSlimNeighbors.Add(slimNeighbor);
-            }
-
-            if (block != null) // Only set fatblock visiblity to false
-                block.Visible = blockSlimNeighbors.Count != GetBlockFaceCount(block);
-
-            if (recursive) // Do set nearby blocks visibility to false.
-                foreach (var slimBlockN in blockSlimNeighbors.Where(slimBlockN => slimBlockN.FatBlock != null))
-                    SetTransparency(slimBlockN, false);
-        }
-
-        private int GetBlockFaceCount(IMyCubeBlock block)
-        {
-            Vector3I blockSize = Vector3I.Abs(block.Max - block.Min) + Vector3I.One;
-            return 2 * (blockSize.X * blockSize.Y + blockSize.Y*blockSize.Z + blockSize.Z*blockSize.X);
-        }
-
-        private Vector3I[] GetSurfacePositions(IMySlimBlock block)
-        {
-            List<Vector3I> surfacePositions = new List<Vector3I>();
-            Vector3I blockSize = Vector3I.Abs(block.Max - block.Min) + Vector3I.One;
-            Vector3I min = block.Min;
-
-            for (int x = -1; x <= blockSize.X; x++)
-            {
-                for (int y = -1; y <= blockSize.Y; y++)
                 {
-                    for (int z = -1; z <= blockSize.Z; z++)
-                    {
-                        // One x, y, z should be at the outside edge of the block. Otherwise don't add to array.
-                        bool xLimit = (x == -1 || x == blockSize.X);
-                        bool yLimit = (y == -1 || y == blockSize.Y);
-                        bool zLimit = (z == -1 || z == blockSize.Z);
-                        if (TernaryXor(xLimit, yLimit, zLimit)) // Avoid checking positions inside the block.
-                            surfacePositions.Add(min + new Vector3I(x, y, z));
-                    }
+                    slimNeighbor.FatBlock.Visible = true;
+                    _culledBlocks[slimNeighbor.CubeGrid].Remove(slimNeighbor.FatBlock);
                 }
             }
-
-            return surfacePositions.ToArray();
-        }
-
-        public static bool TernaryXor(bool a, bool b, bool c)
-        {
-            return (!a && (b ^ c)) || (a && !(b || c));
         }
 
         #endregion

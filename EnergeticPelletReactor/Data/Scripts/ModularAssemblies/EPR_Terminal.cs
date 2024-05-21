e2212b6fd6a22da3015c5d92f92ea63d1fa64765
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-05-21 17:47:36 -0500

    Update EPR_Terminal.cs

--- EnergeticPelletReactor/Data/Scripts/ModularAssemblies/EPR_Terminal.cs
+++ EnergeticPelletReactor/Data/Scripts/ModularAssemblies/EPR_Terminal.cs
@@ -1,7 +1,7 @@
 ﻿using Sandbox.ModAPI;
 using Sandbox.ModAPI.Interfaces.Terminal;
-using System.Collections.Generic;
 using VRage.Utils;
+using System.Collections.Generic;
 
 namespace Scripts.ModularAssemblies
 {
@@ -28,27 +28,24 @@ namespace Scripts.ModularAssemblies
 
         private static void GeneratorCustomControlGetter(IMyTerminalBlock block, List<IMyTerminalControl> controls)
         {
-            if (block?.BlockDefinition.SubtypeId == "EPR_Generator")
+            if (block?.BlockDefinition.SubtypeId == "EPR_Generator" && !ControlsAlreadyAdded(controls))
             {
                 var button1 = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlButton, IMyBatteryBlock>(IdPrefix + "Button1");
                 button1.Title = MyStringId.GetOrCompute("Button 1");
                 button1.Tooltip = MyStringId.GetOrCompute("This is button 1");
                 button1.Action = b => MyAPIGateway.Utilities.ShowNotification("Button 1 pressed", 2000);
-                button1.Visible = b => b.BlockDefinition.SubtypeId == "EPR_Generator";
                 controls.Add(button1);
 
                 var button2 = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlButton, IMyBatteryBlock>(IdPrefix + "Button2");
                 button2.Title = MyStringId.GetOrCompute("Button 2");
                 button2.Tooltip = MyStringId.GetOrCompute("This is button 2");
                 button2.Action = b => MyAPIGateway.Utilities.ShowNotification("Button 2 pressed", 2000);
-                button2.Visible = b => b.BlockDefinition.SubtypeId == "EPR_Generator";
                 controls.Add(button2);
 
                 var button3 = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlButton, IMyBatteryBlock>(IdPrefix + "Button3");
                 button3.Title = MyStringId.GetOrCompute("Button 3");
                 button3.Tooltip = MyStringId.GetOrCompute("This is button 3");
                 button3.Action = b => MyAPIGateway.Utilities.ShowNotification("Button 3 pressed", 2000);
-                button3.Visible = b => b.BlockDefinition.SubtypeId == "EPR_Generator";
                 controls.Add(button3);
             }
         }
@@ -60,15 +57,26 @@ namespace Scripts.ModularAssemblies
 
         private static void InjectorCustomControlGetter(IMyTerminalBlock block, List<IMyTerminalControl> controls)
         {
-            if (block?.BlockDefinition.SubtypeId == "EPR_Injector")
+            if (block?.BlockDefinition.SubtypeId == "EPR_Injector" && !ControlsAlreadyAdded(controls))
             {
                 var button = MyAPIGateway.TerminalControls.CreateControl<IMyTerminalControlButton, IMyCollector>(IdPrefix + "Button");
                 button.Title = MyStringId.GetOrCompute("Inject");
                 button.Tooltip = MyStringId.GetOrCompute("Perform the injection action");
                 button.Action = b => MyAPIGateway.Utilities.ShowNotification("Injector Button pressed", 2000);
-                button.Visible = b => b.BlockDefinition.SubtypeId == "EPR_Injector";
                 controls.Add(button);
             }
+        }
+
+        private static bool ControlsAlreadyAdded(List<IMyTerminalControl> controls)
+        {
+            foreach (var control in controls)
+            {
+                if (control.Id.StartsWith(IdPrefix))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         // Add similar methods for other block types as needed

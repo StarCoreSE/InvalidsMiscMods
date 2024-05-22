95f0cf18529542c4b679a3f399d56d5d964ae12f
InvalidArgument3 <InvalidArgument3@gmail.com>
2024-05-22 13:59:47 -0500

    here we go fish

---
+++ OpenCycleFission/Data/Scripts/ModularAssemblies/OCFi_Manager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Scripts.ModularAssemblies.Communication;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+
+namespace Scripts.ModularAssemblies
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+    internal class OCFiManager : MySessionComponentBase
+    {
+        public ModularDefinitionApi ModularApi = new ModularDefinitionApi();
+        public Dictionary<int, OCFi_ReactorLogic> Reactors = new Dictionary<int, OCFi_ReactorLogic>();
+
+        private bool _didRegisterAssemblyClose = false;
+
+        public override void LoadData()
+        {
+            ModularApi.Init(ModContext);
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            if (!ModularApi.IsReady)
+                return;
+
+            if (!_didRegisterAssemblyClose)
+            {
+                ModularApi.AddOnAssemblyClose(assemblyId => Reactors.Remove(assemblyId));
+                _didRegisterAssemblyClose = true;
+            }
+
+            foreach (var reactor in Reactors.Values)
+                reactor.UpdateAfterSimulation();
+
+            var systems = ModularApi.GetAllAssemblies();
+            foreach (var reactor in Reactors.Values.ToList())
+                if (!systems.Contains(reactor.PhysicalAssemblyId))
+                    Reactors.Remove(reactor.PhysicalAssemblyId);
+        }
+
+        public void OnPartAdd(int PhysicalAssemblyId, IMyCubeBlock NewBlockEntity, bool IsBaseBlock)
+        {
+            if (!Reactors.ContainsKey(PhysicalAssemblyId))
+                Reactors.Add(PhysicalAssemblyId, new OCFi_ReactorLogic(PhysicalAssemblyId));
+
+            Reactors[PhysicalAssemblyId].AddPart(NewBlockEntity);
+        }
+
+        public void OnPartRemove(int PhysicalAssemblyId, IMyCubeBlock BlockEntity, bool IsBaseBlock)
+        {
+            if (!Reactors.ContainsKey(PhysicalAssemblyId))
+                return;
+
+            if (!IsBaseBlock)
+                Reactors[PhysicalAssemblyId].RemovePart(BlockEntity);
+        }
+    }
+}
---
+++ OpenCycleFission/Data/Scripts/ModularAssemblies/OCFi_Reactor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Sandbox.ModAPI;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Utils;
+
+namespace Scripts.ModularAssemblies
+{
+    public class OCFi_ReactorLogic : MySessionComponentBase
+    {
+        public int PhysicalAssemblyId { get; private set; }
+        private List<IMyCubeBlock> _parts = new List<IMyCubeBlock>();
+
+        private float temperature = 0f;
+        private const float maxTemperature = 5000f;
+        private const float temperatureIncrement = 50f;
+
+        public OCFi_ReactorLogic(int physicalAssemblyId)
+        {
+            PhysicalAssemblyId = physicalAssemblyId;
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            UpdateTemperature();
+            ShowTemperatureNotification();
+        }
+
+        public void AddPart(IMyCubeBlock block)
+        {
+            _parts.Add(block);
+        }
+
+        public void RemovePart(IMyCubeBlock block)
+        {
+            _parts.Remove(block);
+        }
+
+        private void UpdateTemperature()
+        {
+            if (temperature < maxTemperature)
+            {
+                temperature += temperatureIncrement;
+            }
+        }
+
+        private void ShowTemperatureNotification()
+        {
+            MyAPIGateway.Utilities.ShowNotification($"Reactor Temperature: {temperature} K", 1000 / 60);
+        }
+    }
+}

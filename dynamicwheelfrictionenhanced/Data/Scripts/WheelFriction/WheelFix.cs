29e4e39f86823a4c2de381bda66bb5e530ca0ae2
MuzzMuzz <48306411+MuzzMuzz@users.noreply.github.com>
2024-08-10 21:01:09 -0600

    Logging features

--- dynamicwheelfrictionenhanced/Data/Scripts/WheelFriction/WheelFix.cs
+++ dynamicwheelfrictionenhanced/Data/Scripts/WheelFriction/WheelFix.cs
@@ -1,19 +1,25 @@
 ﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.Entities;
 using Sandbox.Game.Entities.Cube;
 using Sandbox.ModAPI;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using VRage.Game;
 using VRage.Game.Components;
+using VRage.Game.ModAPI;
 using VRage.Utils;
 using VRageMath;
 
 namespace WheelFix
 {
-    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
     public class WheelFixSession : MySessionComponentBase
     {
         public static bool IsEnabled = true;
         private static bool _isHandlerRegistered = false;
+        private static List<WheelFix> _activeWheels = new List<WheelFix>();
+        private static int _updateCounter = 0;
 
         public override void LoadData()
         {
@@ -34,6 +40,31 @@ namespace WheelFix
             }
         }
 
+        public override void UpdateAfterSimulation()
+        {
+            base.UpdateAfterSimulation();
+
+            if (!IsEnabled || ++_updateCounter % 60 != 0) return;
+
+            var player = MyAPIGateway.Session?.Player;
+            if (player == null || player.Character == null) return;
+
+            var playerPosition = player.Character.PositionComp.GetPosition();
+
+            var nearestWheels = _activeWheels
+                .OrderBy(w => Vector3D.DistanceSquared(w.GetPosition(), playerPosition))
+                .Take(4)
+                .ToList();
+
+            for (int i = 0; i < nearestWheels.Count; i++)
+            {
+                var wheel = nearestWheels[i];
+                var distance = Vector3D.Distance(wheel.GetPosition(), playerPosition);
+                MyAPIGateway.Utilities.ShowNotification($"Wheel {i + 1}: {wheel.GetName()} - Distance: {distance:F2}m", 3000, MyFontEnum.White);
+                wheel.DisplayDebugInfo();
+            }
+        }
+
         protected override void UnloadData()
         {
             if (_isHandlerRegistered)
@@ -42,6 +73,19 @@ namespace WheelFix
                 _isHandlerRegistered = false;
             }
         }
+
+        public static void RegisterWheel(WheelFix wheel)
+        {
+            if (!_activeWheels.Contains(wheel))
+            {
+                _activeWheels.Add(wheel);
+            }
+        }
+
+        public static void UnregisterWheel(WheelFix wheel)
+        {
+            _activeWheels.Remove(wheel);
+        }
     }
 
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_MotorSuspension), false)]
@@ -49,11 +93,13 @@ namespace WheelFix
     {
         private IMyMotorSuspension _suspension;
         private int _debugCounter = 0;
+
         public override void OnAddedToContainer()
         {
             base.OnAddedToContainer();
             this.NeedsUpdate |= VRage.ModAPI.MyEntityUpdateEnum.EACH_FRAME;
             _suspension = (IMyMotorSuspension)this.Entity;
+            WheelFixSession.RegisterWheel(this);
         }
 
         public const double ResistanceCoefficient = 100d;
@@ -84,20 +130,31 @@ namespace WheelFix
 
             var smoothingForce = TerrainSmoothingFactor * _suspension.PositionComp.WorldMatrixRef.Up;
             grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_IMPULSE_AND_WORLD_ANGULAR_IMPULSE, smoothingForce, null, null);
-
-            if (_debugCounter++ % 60 == 0)
-            {
-                MyAPIGateway.Utilities.ShowNotification($"Suspension: {_suspension.CustomName}", 1000);
-                MyAPIGateway.Utilities.ShowNotification($"Base Friction: {num:F2}", 1000);
-                MyAPIGateway.Utilities.ShowNotification($"Added Friction: {num2:F2}", 1000);
-                MyAPIGateway.Utilities.ShowNotification($"Total Friction: {grid.Physics.Friction:F2}", 1000);
-                MyAPIGateway.Utilities.ShowNotification($"Smoothing Force: {smoothingForce.Length():F2}", 1000);
-            }
         }
 
-        public override void UpdateAfterSimulation()
+        public void DisplayDebugInfo()
         {
-            base.UpdateAfterSimulation();
+            var grid = _suspension?.TopGrid;
+            if (grid?.Physics == null) return;
+
+            MyAPIGateway.Utilities.ShowNotification($"Base Friction: {grid.Physics.Friction:F2}", 3000, MyFontEnum.White);
+            MyAPIGateway.Utilities.ShowNotification($"Smoothing Force: {TerrainSmoothingFactor:F2}", 3000, MyFontEnum.White);
+        }
+
+        public Vector3D GetPosition()
+        {
+            return _suspension.GetPosition();
+        }
+
+        public string GetName()
+        {
+            return _suspension.CustomName;
+        }
+
+        public override void Close()
+        {
+            WheelFixSession.UnregisterWheel(this);
+            base.Close();
         }
     }
 }
